8577aaaead49cdf6e8e5c5cecb0437e2cca52605
Siddhi0308 <81307598+Siddhi0308@users.noreply.github.com>
2022-11-06 18:31:54 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCGBY4j73di5GxgbyJWHuw9llUtNXjX4aU",
+    authDomain: "let-s-chat-web-app-f0678.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-f0678-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-web-app-f0678",
+    storageBucket: "let-s-chat-web-app-f0678.appspot.com",
+    messagingSenderId: "94345614037",
+    appId: "1:94345614037:web:c99dcf84be6e687583885c"
+  };
+  const app = initializeApp(firebaseConfig);
+
